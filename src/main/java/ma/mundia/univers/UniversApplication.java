dd559c77027c92174a284d7a2a0a70f8b4444643
Youssef Ait Said <saidyoussef642@gmail.com>
2025-10-25 13:06:10 +0100

    Second commit

--- src/main/java/ma/mundia/univers/UniversApplication.java
+++ src/main/java/ma/mundia/univers/UniversApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.List;
+
 @SpringBootApplication
 public class UniversApplication implements CommandLineRunner {
 
@@ -20,10 +22,47 @@ public class UniversApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        productRepository.save(new Product(null , "Hp" , 4500 , 10));
-        productRepository.save(new Product(null , "Dell" , 5000 , 10));
-        productRepository.save(new Product(null , "Lenovo" , 3900 , 10));
-        productRepository.save(new Product(null , "Toshiba" , 4100 , 10));
+//        productRepository.save(new Product(null , "Hp" , 4500 , 10));
+//        productRepository.save(new Product(null , "Dell" , 5000 , 10));
+//        productRepository.save(new Product(null , "Lenovo" , 3900 , 10));
+//        productRepository.save(new Product(null , "Toshiba" , 4100 , 10));
+
+        List<Product> all = productRepository.findAll();
+
+        all.forEach(p -> System.out.println(p.toString()));
+        Product product =   productRepository.findById(Long.valueOf(1)).get();
+        System.out.println("************");
+        System.out.println(product.getId());
+        System.out.println(product.getName());
+        System.out.println(product.getQuantity());
+        System.out.println("************");
+
+        List<Product> productList = productRepository.findByNameContains("l");
+        productList.forEach(p ->{
+            System.out.println(p);
+        });
+
+        System.out.println("---------------");
+
+        List<Product> productList2 = productRepository.serach("%L%");
+        productList2.forEach(p ->{
+            System.out.println(p);
+        });
+
+        System.out.println("---------------");
+
+        List<Product> productList3 = productRepository.findByPriceGreaterThan(4000.0);
+        productList3.forEach(p ->{
+            System.out.println(p);
+        });
+
+        System.out.println("---------------");
+
+        List<Product> productList4 = productRepository.serachByPrice(4000.0);
+        productList4.forEach(p ->{
+            System.out.println(p);
+        });
+
 
     }
 }
--- src/main/java/ma/mundia/univers/repository/ProductRepository.java
+++ src/main/java/ma/mundia/univers/repository/ProductRepository.java
@@ -2,6 +2,21 @@ package ma.mundia.univers.repository;
 
 import ma.mundia.univers.entities.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface ProductRepository extends JpaRepository<Product,Long> {
+//methode 1 : methode de jpa :
+    List<Product> findByNameContains(String mc); // 1ere methode : Spring sait automatiquement ce que nous voulons seulement du nom du methode et parametre
+
+    List<Product> findByPriceGreaterThan(double price);
+
+    // mwthode 2 : methode classique par querry sql :
+    @Query("select p from Product p where p.name like :x") // 2eme methode
+    List<Product> serach(@Param("x") String mc);
+
+    @Query("select p from Product p where p.price> :x")
+    List<Product> serachByPrice(@Param("x") double price);
 }
